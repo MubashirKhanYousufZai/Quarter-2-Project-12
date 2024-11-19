3052f2df0f75fcc719bd168d35a13129848203b6
Mubashir Khan <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-11-20 03:47:02 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div className="grid grid-rows-[100px_200px_200px_100px] grid-cols-3 gap-1">
+     <div className="bg-emerald-600 col-span-3 text-black font-bold text-center content-center">HOME ABOUT CONTACT PROFILE</div>
+     <div className="bg-cyan-600 row-span-2 text-black font-bold text-center content-center">
+     <ol>Project Overview 
+      <li>Summary</li>
+      <li>Milestone</li>
+      <li>Status Update</li>
+     </ol>
+     <ol>Task Managment
+      <li>Create New Task</li>
+      <li>My Task</li>
+      <li>Assignments</li>
+     </ol>
+</div>
+     <div className="bg-pink-400 col-span-2 text-black font-bold text-center content-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur rem commodi aliquid animi libero incidunt, quos, nobis quis dignissimos accusantium error sint molestias mollitia eius aperiam! Distinctio, in. Veritatis, deserunt.</div>
+     <div className="bg-rose-600 col-span-1 text-black font-bold text-center content-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum fuga maxime excepturi, autem enim dolore iure obcaecati illo delectus harum repellendus deserunt, quo tempora, similique exercitationem est! Eius, eveniet nesciunt?</div>
+     <div className="bg-orange-500 col-span-1 text-black font-bold text-center content-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, voluptatibus. Eligendi laboriosam ipsum ipsa suscipit quia consequatur perferendis, est obcaecati dolores animi placeat, sit cumque, facilis inventore nesciunt nemo illo?</div>
+     <div className="bg-gray-500 col-span-3 text-black font-bold text-center content-center">🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄🙄</div>
+  </div>
+  );
+}
